589e6eccf65bf2683f3e860c56c2fde5c32193d1
Genokrad <zeeper90@gmail.com>
2022-09-11 22:06:14 +0300

    no scroll

--- js/menu.js
+++ js/menu.js
@@ -3,10 +3,12 @@
     openMenuBtn: document.querySelector('.menu-open'),
     closeMenuBtn: document.querySelector('.mob-menu__close'),
     menu: document.querySelector('.mob-menu'),
+    body: document.querySelector('body'),
   };
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
