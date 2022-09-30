526bd43f7d7c32e51a7e17ae9402888e7eee81ea
Genokrad <zeeper90@gmail.com>
2022-09-30 20:59:12 +0300

    close menu

--- js/menu.js
+++ js/menu.js
@@ -4,10 +4,15 @@
     closeMenuBtn: document.querySelector('.mob-menu__close'),
     menu: document.querySelector('.mob-menu'),
     body: document.querySelector('body'),
+    ul: document.querySelectorAll('.navig-mobile__list'),
   };
+
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
   function toggleMenu() {
+    for (let i = 0; i < refs.ul[0].children.length; i++) {
+      refs.ul[0].children[i].addEventListener('click', toggleMenu);
+    }
     refs.menu.classList.toggle('is-hidden');
     refs.body.classList.toggle('no-scroll');
   }
